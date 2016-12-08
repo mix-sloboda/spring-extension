525d365d567c6609e0d9d05a3937299af93b34ba
mix-sloboda <mix.sloboda@gmail.com>
2016-12-08 17:13:26 +0300

    replace class configurator with scan packages

--- src/main/scala/com/directual/extension/spring/SpringCtxExtImpl.scala
+++ src/main/scala/com/directual/extension/spring/SpringCtxExtImpl.scala
@@ -2,20 +2,21 @@ package com.directual.extension.spring
 
 import akka.actor.{ActorSystem, Extension, Props, _}
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import collection.JavaConverters._
 
 /**
  * The Extension implementation.
  */
 class SpringCtxExtImpl(val system:ExtendedActorSystem) extends Extension {
 
-  private[this] val clazz = Class.forName(system.settings.config.getString("spring-context.extension.application-config.clazz"))
-  private[this] val applicationContext = new AnnotationConfigApplicationContext(clazz) {
+  private[this] val packagesToScan = system.settings.config.getStringList("spring-context.extension.packages-to-scan").asScala
+  private[this] val applicationContext = new AnnotationConfigApplicationContext(packagesToScan:_*) {
     override def setClassLoader(classLoader: ClassLoader): Unit = system.dynamicAccess.classLoader
   }
 
   system.log.info("Created SpringContext Extension")
 
-  def springContext = applicationContext
+  def springContext: AnnotationConfigApplicationContext = applicationContext
 
   def props(actorBeanName: String): Props = Props(classOf[ActorProducer], applicationContext, actorBeanName)
 
@@ -27,9 +28,7 @@ class SpringCtxExtImpl(val system:ExtendedActorSystem) extends Extension {
 
 object SpringCtxExt extends AbstractExtensionId[SpringCtxExtImpl] with ExtensionIdProvider {
 
-  override def createExtension(system: ExtendedActorSystem) = {
-    new SpringCtxExtImpl(system)
-  }
+  override def createExtension(system: ExtendedActorSystem) = new SpringCtxExtImpl(system)
 
   override def lookup() = SpringCtxExt
 
